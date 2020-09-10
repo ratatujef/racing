ba3a04a6a31ef0e2de59bc9c4b6042b3a4bf9e67
ratatujef <ratatujef@gmail.com>
2020-09-10 18:09:49 +0300

    +

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,46 @@
-document.addEventListener('DOMContentLoaded', function(){
+
     const score=document.querySelector('.score'),
             gameArea=document.querySelector('.gameArea'),
             start=document.querySelector('.start'),
             car=document.createElement('div'),
             keys={
-                arrowLeft: false,
-                arrowrRight: false,
-                arrowUp: false,
-                arrowDown: false,
+                ArrowLeft: false,
+                ArrowRight: false,
+                ArrowUp: false,
+                ArrowDown: false,
             },
             setting={
-                scope: 0,
+                score: 0,
                 speed: 3,
                 start: false,
             };
     start.addEventListener('click', startGame);
     document.addEventListener('keydown',startRun);
-    document.addEventListener('keydown',stopRun);
+    document.addEventListener('keyup',stopRun);
 
     function startGame(){
         start.classList.toggle('hide');
         setting.start=true;
-        requestAnimationFrame(playGame);
         gameArea.appendChild(car);
         car.classList.add ('car');
         setting.x=car.offsetLeft;
-        
+        requestAnimationFrame(playGame);
+        console.log(setting.x);
 
     }
     function playGame(){
-        console.log('working!');
         if (setting.start){
-            if(keys.arrowLeft){
-                setting.x++;
+
+            setting.score+=setting.speed;
+            score.textContent=`Очки: ${setting.score} `;
+
+            if(keys.ArrowLeft&&setting.x>0){
+                console.log('Работает');
+                setting.x-=setting.speed;
+                
             }
-            if(keys.arrowRight){
-                setting.x--;
+            if(keys.ArrowRight&&setting.x<(gameArea.offsetWidth-car.offsetWidth)){
+                setting.x+=setting.speed;
             }
 
             car.style.left=setting.x+'px';
@@ -45,10 +50,9 @@ document.addEventListener('DOMContentLoaded', function(){
     function startRun(e){
         e.preventDefault();
         keys[e.key]=true;
-    }
+        console.log(keys[e.key]+" а в объекте "+keys.ArrowLeft);
+        }
     function stopRun(e){
         e.preventDefault();
-        
         keys[e.key]=false;
     }
-});
